6ad655421a59e3e6dea7df03c54e6225a2601427
Jon Burdon <jb@burdonworld.net>
2019-06-10 19:36:20 +0100

    Click Button One to toggle Paragraph

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
  
+	// $(".bottom_button1").click(function() {
+	// 	$(".bottom_button1").hide("slow");
+	// });
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+	$("#buttoneffects1").click(function() {
+		$("#par1").toggle("slow");
 	});
-	$("#stream2_btn").on("click", function() {
+
+ 	$("#stream1_btn").click(function() {
+		 $(".stream1").toggle();
+		 $(".stream1").toggle(1000);
+		 $(".stream1").show();
+		 $(".stream1").removeClass('highlight_stream');
+		 $(".stream2").removeClass('highlight_stream');
+		 $(".stream3").removeClass('highlight_stream');
+		$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").mouseenter(function() {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
@@ -19,6 +29,16 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
+	$("p").on("click", function() {
+		$("p").css("color","RebeccaPurple");
+	});
 
+	$("h2").mouseenter(function() {
+		$("h2").css("background-color","LightCoral");
+	});
+
+	$("img").click(function() {
+		$("p").show();
+	});
 
 }); 
