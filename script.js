4673ac0c39dd4cfcc227c5f0f7cc5607e3e48e27
Jon Burdon <jb@burdonworld.net>
2019-06-11 09:42:26 +0100

    Update script.js to standardise button behaviour

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ $(document).ready(function() {
 	});
 
 	$("#buttoneffects2").mouseenter(function() {
-		$("#par2").fadeTo("slow", 0.5);
+		$("#par2").fadeTo("slow", 0.5).css("background-color","RebeccaPurple");
 	});
 
 	$("#buttoneffects2").mouseleave(function() {
-		$("#par2").fadeTo("slow", 1.0);
+		$("#par2").fadeTo("slow", 1.0).css("background-color","White");
 	});
 
  	$("#stream1_btn").click(function() {
-		 $(".stream1").toggle();
-		 $(".stream1").toggle(1000);
-		 $(".stream1").show();
+		//  $(".stream1").toggle();
+		//  $(".stream1").toggle(1000);
+		//  $(".stream1").show();
 		 $(".stream1").removeClass('highlight_stream');
 		 $(".stream2").removeClass('highlight_stream');
 		 $(".stream3").removeClass('highlight_stream');
 		$(".stream1").addClass('highlight_stream');
 	});
-	$("#stream2_btn").mouseenter(function() {
+	$("#stream2_btn").click(function() {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
